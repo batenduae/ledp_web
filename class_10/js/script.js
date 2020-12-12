b01e8b96a3b9327e6c70ea8922dd01ebc759c7b3
Md. Abdul Baten <55513652+batenduae@users.noreply.github.com>
2020-12-12 14:27:22 +0600

    class_11

---
+++ class_10/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        center:true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        items: 2,
+        
+    });
+});
